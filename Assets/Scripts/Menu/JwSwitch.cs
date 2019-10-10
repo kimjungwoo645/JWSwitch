8b17c6224c9263fa22536e8e7fc7213329237a3c
Jeung Woo <odav12@naver.com>
2019-10-10 15:47:02 +0900

    프로젝트시작
    JSWitch

---
+++ Assets/Scripts/Menu/JwSwitch.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class JwSwitch : MonoBehaviour
+{
+    bool Bgm; //true = On , false = Off
+    int OnOffLimit = 0;
+    public float Speed;
+    public float time;
+
+    public Action<bool> switchIsOnAction;
+    public RectTransform handleRectTransform;
+    public Image image;
+
+    private void Start()
+    {
+        Bgm = true;
+        handleRectTransform.anchoredPosition = new Vector2(-25, 0);
+        image.color = Color.blue;
+    }
+
+    //스위치 On Off 동작
+
+    public void OnClickBgmSwitch()
+    {
+        if(Bgm == true) //off함수
+        {
+            StartCoroutine(OffSwitch());
+        }
+        else if(Bgm == false) //on함수
+        {
+            StartCoroutine(OnSwitch()); 
+        }
+        switchIsOnAction(Bgm);
+
+        IEnumerator OnSwitch()
+        {
+            if (OnOffLimit == 1)
+            {
+                OnOffLimit -= 1;
+                float move = 25.0f;
+                while (move > -25)
+                {
+                    move -= Speed;
+                    handleRectTransform.anchoredPosition = new Vector2(move, 0);
+                    yield return new WaitForSeconds(time);
+                }
+                image.color = Color.blue;
+                Bgm = true;
+            }
+        }
+        IEnumerator OffSwitch()
+        {
+            if (OnOffLimit == 0)
+            {
+                OnOffLimit += 1;
+                float move = -25.0f;
+                while (move < 25)
+                {
+                    move += Speed;
+                    handleRectTransform.anchoredPosition = new Vector2(move, 0);
+                    yield return new WaitForSeconds(time);
+                }
+                image.color = Color.black;
+                Bgm = false;
+            }
+        }
+    }
+}
---
+++ Assets/SwitchMaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SwitchMaster : MonoBehaviour
+{
+    public JwSwitch bgmSwitch;
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        bgmSwitch.switchIsOnAction = (Bgm) =>
+        {
+            if (Bgm == true)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Stop();
+            }
+        };
+    }
+
+    void Update()
+    {
+        
+    }
+}
